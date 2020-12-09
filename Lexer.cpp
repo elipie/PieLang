b30d58a4920ec43ab7fdfce3644883fbc85deb6d
Eli <72884616+elipie@users.noreply.github.com>
2020-12-09 10:14:00 -0600

    Create Lexer.cpp & added base code

---
+++ Lexer.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+struct Token{
+  std::string type;
+  std::string value; // This means i will turn it into a string example: 2 would turn into "2"
+  // other 
+  int line;
+};  
+enum class Types{
+  Number,
+  DoubleQuote,
+  Quote,
+  Initializer,
+  LessThan,
+  LargerThan,
+  Equal,
+  SlashOrComment,
+  AtOrReturn,
+  Pipe,
+  LParen,
+  RParen,
+  LCurly,
+  RCurly,
+  RBracket,
+  LBracket,
+  Colon,
+  SemiColon,
+  Star,
+  Plus,
+  Caret,
+  //more later.
+};
+class lexer{
+  
+};
