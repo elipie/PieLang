bd9eef4c1c2a048c65146a9c20ba31312970bb4e
Eli <elipiechocki@gmail.com>
2020-12-10 15:31:30 +0000

    Fixed Filestream bug

--- setup.cpp
+++ setup.cpp
@@ -1,6 +1,6 @@
 #include <fstream>
 int setup(){
-  ofstream main;
+  std::ofstream main;
   main.open("tests.pie"); // once release, we change this to main.pie
-  
+  return 0; // if tests.pie does not exist, throw an error
 }
