ba68aa95bc6668c2b3cb720213bedfb2e403aec8
Eli <72884616+elipie@users.noreply.github.com>
2020-12-16 14:47:18 -0600

    Delete setup.cpp

--- setup.cpp
+++
@@ -1,6 +0,0 @@
-#include <fstream>
-int setup(){
-  std::ifstream main;
-  main.open("tests.pie"); // once release, we change this to main.pie
-  return 0; // if tests.pie does not exist, throw an error
-}
